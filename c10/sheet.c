fff2f6a15b6c14def1a01e8f0b20f2c776010dad
ziruiLiu-g <ziruiliu98@gmail.com>
2024-09-02 15:22:54 -0700

    C10 Optimization 2

--- c10/sheet.c
+++ c10/sheet.c
@@ -100,36 +100,51 @@ void sheet_updown(struct Shtctl *ctl, struct Sheet *sht, int height) {
 }
 
 void sheet_refreshsub(struct Shtctl *ctl, int vx0, int vy0, int vx1, int vy1) {
-    int h, bx, by, vx, vy;
-    unsigned char *buf, c, *vram = ctl -> vram;
-    struct Sheet *sht;
+  unsigned char *buf, *vram = ctl->vram;
+  struct Sheet *sht;
 
-    for (h = 0; h <= ctl->top; h++) {
-        sht = ctl->sheets[h];
-        buf = sht->buf;
+  for (int h = 0; h <= ctl->top; h++) {
+    sht = ctl->sheets[h];
+    buf = sht->buf;
+    // 使用vx0 ~ vy1，对bx0 ~ by1进行倒推
+    int bx0 = vx0 - sht->vx0;
+    int by0 = vy0 - sht->vy0;
+    int bx1 = vx1 - sht->vx0;
+    int by1 = vy1 - sht->vy0;
 
-        for (by = 0; by < sht->bysize; by++) {
-            vy = sht->vy0 + by;
-            for (bx = 0; bx < sht->bxsize; bx++) {
-                vx = sht->vx0 + bx;
-
-                if (vx0 <= vx && vx < vx1 && vy0 <= vy && vy < vy1) {
-                    c = buf[by * sht->bxsize + bx];
-                    if (c != sht->col_inv) {
-                        vram[vy * ctl->xsize + vx] = c;
-                    }
-                }
-            }
-        }
+    if (bx0 < 0) {
+      bx0 = 0;
     }
+    if (by0 < 0) {
+      by0 = 0;
+    }
+    if (bx1 > sht->bxsize) {
+      bx1 = sht->bxsize;
+    }
+    if (by1 > sht->bysize) {
+      by1 = sht->bysize;
+    }
+
+    for (int by = by0; by < by1; by++) {
+      int vy = sht->vy0 + by;
+      for (int bx = bx0; bx < bx1; bx++) {
+        int vx = sht->vx0 + bx;
+        unsigned char c = buf[by * sht->bxsize + bx];
+        if (c != sht->col_inv) {
+          vram[vy * ctl->xsize + vx] = c;
+        }
+      }
+    }
+  }
 }
 
-void sheet_refresh(struct Shtctl *ctl, struct Sheet *sht, int bx0, int by0, int bx1, int by1) {
-    if (sht->height >= 0) {
-        sheet_refreshsub(ctl, sht->vx0 + bx0, sht->vy0 + by0, sht->vx0 + bx1, sht->vy0 + by1);
-    }
-
-    return;
+void sheet_refresh(struct Shtctl *ctl, struct Sheet *sht, int bx0, int by0,
+                   int bx1, int by1) {
+  if (sht->height >= 0) {
+    // 如果正在显示，则按新图层的信息刷新画面
+    sheet_refreshsub(ctl, sht->vx0 + bx0, sht->vy0 + by0, sht->vx0 + bx1,
+                     sht->vy0 + by1);
+  }
 }
 
 void sheet_slide(struct Shtctl *ctl, struct Sheet *sht, int vx0, int vy0) {
