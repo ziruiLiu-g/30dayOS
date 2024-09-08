d1c33cfad16bd43ababc5f83b704b061babf936c
ziruiLiu-g <ziruiliu98@gmail.com>
2024-09-08 15:04:33 -0700

    C13 Finished

---
+++ c13/bootpack.c
@@ -0,0 +1,171 @@
+#include <stdio.h>
+
+#include "bootpack.h"
+#include "desctbl.h"
+#include "graphic.h"
+#include "int.h"
+#include "io.h"
+#include "keyboard.h"
+#include "fifo.h"
+#include "memory.h"
+#include "mouse.h"
+#include "sheet.h"
+#include "window.h"
+#include "timer.h"
+
+int main(void)
+{
+    struct BootInfo *binfo = (struct BootInfo *)ADR_BOOTINFO;
+    struct MemMan *memman = (struct MemMan *) MEMMAN_ADDR;
+    unsigned int memtotal, count = 0;
+    struct MouseDec mdec;
+    char s[40], mcursor[256];
+    struct Shtctl *shtctl;
+    struct Sheet *sht_back, *sht_mouse, *sht_win;
+    unsigned char *buf_back, buf_mouse[256], *buf_win;
+
+    struct FIFO32 fifo;
+    int fifobuf[128], data;
+    struct Timer *timer, *timer2, *timer3;
+
+    init_gdtidt();
+    init_pic(); // GDT/IDT完成初始化，开放CPU中断
+    io_sti(); // IF(interrupt flag) 变为1， cpu可以开始接受外部指令
+    
+
+    fifo32_init(&fifo, 128, fifobuf);
+    init_keyboard(&fifo, 256);
+    enable_mouse(&fifo, 512, &mdec);
+
+    init_pit();
+
+    io_out8(PIC0_IMR, 0xf8); // 开放PIC1以及键盘中断
+    io_out8(PIC1_IMR, 0xef); // 开放鼠标中断
+
+
+    timer = timer_alloc();
+    timer_init(timer, &fifo, 5);
+    timer_set_timer(timer, 500);
+
+    timer2 = timer_alloc();
+    timer_init(timer2, &fifo, 3);
+    timer_set_timer(timer2, 300);
+
+    timer3 = timer_alloc();
+    timer_init(timer3, &fifo, 1);
+    timer_set_timer(timer3, 50);
+
+    memtotal = memtest(0x00400000, 0xbfffffff);
+    memman_init(memman);
+    // 测试时发现会造成错误（原因未知），所以改为由0x00010000开始
+    memman_free(memman, 0x00010000, 0x009e000); /* 0x00010000 - 0x0009efff */
+    memman_free(memman, 0x00400000, memtotal - 0x00400000);
+
+    init_palette();
+
+    shtctl = shtctl_init(memman, binfo->vram, binfo->scrnx, binfo->scrny);
+    sht_back = sheet_alloc(shtctl);
+    sht_mouse = sheet_alloc(shtctl);
+    sht_win = sheet_alloc(shtctl);
+    buf_back =
+        (unsigned char *)memman_alloc_4k(memman, binfo->scrnx * binfo->scrny);
+    buf_win = (unsigned char *)memman_alloc_4k(memman, 160 * 52);
+    sheet_setbuf(sht_back, buf_back, binfo->scrnx, binfo->scrny,
+                -1);                               // 没有透明色
+    sheet_setbuf(sht_mouse, buf_mouse, 16, 16, 99); // 透明色号99
+    sheet_setbuf(sht_win, buf_win, 160, 52, -1);
+    init_screen8(buf_back, binfo->scrnx, binfo->scrny);
+    init_mouse_cursor8(buf_mouse, 99); // 背景色号99
+    make_window8(buf_win, 160, 52, "Counter");
+
+    sheet_slide(sht_back, 0, 0);
+
+    int mx = (binfo->scrnx - 16) / 2;
+    int my = (binfo->scrny - 28 - 16) / 2;
+
+    sheet_slide(sht_mouse, mx, my);
+    sheet_slide(sht_win, 80, 72);
+    sheet_updown(sht_back, 0);
+    sheet_updown(sht_win, 1);
+    sheet_updown(sht_mouse, 2);
+
+    // put_block8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+    sprintf(s, "(%3d, %3d)", mx, my);
+    put_fonts8_asc(buf_back, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
+    sprintf(s, "memory %dMB, free: %dKB", memtotal / (1024 * 1024),
+            memman_total(memman) / 1024);
+    put_fonts8_asc(buf_back, binfo->scrnx, 0, 32, COL8_FFFFFF, s);
+    sheet_refresh(sht_back, 0, 0, binfo->scrnx, 48);
+
+    for (;;)
+    {
+        count++;
+        // sprintf(s, "%010d", timerctl.count);
+        // put_fonts8_asc_sht(sht_win, 40, 28, COL8_000000, COL8_C6C6C6, s, 10);
+
+
+        io_cli();
+        if (fifo32_status(&fifo) == 0) {
+            io_stihlt();
+        } else {
+            data = fifo32_get(&fifo);
+            io_sti();
+
+            if (256 <= data && data <= 511) {
+                sprintf(s, "%02X", data - 256);
+                put_fonts8_asc_sht(sht_back, 0, 16, COL8_FFFFFF, COL8_008484, s, 2);
+            } else if (512 <= data && data <= 767) {
+                if (mouse_decode(&mdec, data - 512)) {
+                    sprintf(s, "[lcr %4d %4d]", mdec.x, mdec.y);
+                    if ((mdec.btn & 0x01) != 0) {
+                        s[1] = 'L';
+                    }
+                    if ((mdec.btn & 0x02) != 0) {
+                        s[3] = 'R';
+                    }
+                    if ((mdec.btn & 0x04) != 0) {
+                        s[2] = 'C';
+                    }
+                    put_fonts8_asc_sht(sht_back, 32, 16, COL8_FFFFFF, COL8_008484, s, 15);
+                    mx += mdec.x;
+                    my += mdec.y;
+                    if (mx < 0) {
+                        mx = 0;
+                    }
+                    if (my < 0) {
+                        my = 0;
+                    }
+                    if (mx > binfo->scrnx - 1) {
+                        mx = binfo->scrnx - 1;
+                    }
+                    if (my > binfo->scrny - 1) {
+                        my = binfo->scrny - 1;
+                    }
+
+                    sprintf(s, "[%3d %3d]", mx, my);
+                    put_fonts8_asc_sht(sht_back, 0, 0, COL8_FFFFFF, COL8_008484, s, 10);
+                    sheet_slide(sht_mouse, mx, my);
+                }
+            } else if (data == 5) {
+                    put_fonts8_asc_sht(sht_back, 0, 64, COL8_FFFFFF, COL8_008484, "5[SEC]", 7);
+                    sprintf(s, "%010d", count);
+                    put_fonts8_asc_sht(sht_win, 40, 28, COL8_000000, COL8_C6C6C6, s, 10);
+            } else if (data == 3) {
+                put_fonts8_asc_sht(sht_back, 0, 80, COL8_FFFFFF, COL8_008484, "3[SEC]", 7);
+                count = 0;
+            } else if (data == 1) {
+                timer_init(timer3, &fifo, 0);
+                box_fill8(buf_back, binfo->scrnx, COL8_FFFFFF, 8, 96, 15, 111);
+                timer_set_timer(timer3, 50);
+                sheet_refresh(sht_back, 8, 96, 16, 112);     
+            } else if (data == 0) {
+                timer_init(timer3, &fifo, 1);
+                box_fill8(buf_back, binfo->scrnx, COL8_008484, 8, 96, 15, 111);   
+                timer_set_timer(timer3, 50);
+                sheet_refresh(sht_back, 8, 96, 16, 112);      
+            }
+        }
+    }
+
+    return 0;
+}
